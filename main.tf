2c8220cf2675d4af21c2b68a2a83b639cabfacbc
paul <paul.dohou@slalom.com>
2022-02-27 22:13:15 -0500

    Added main.tf

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "tf_test" {
+  name = "tfmainrg"
+  location = "East US"
+}
+
+resource "azurerm_container_group" "tfcg_test" {
+  name                  = "weatherapi"
+  location              = azurerm_resource_group.tf_test.location
+  resource_group_name   = azurerm_resource_group.tf_test.name
+
+  ip_address_type       = "public"
+  dns_name_label        = "agbonnon10dns"
+  os_type               = "linux"
+
+  container {
+    name                = "weatherapi"
+    image               = "agbonnon10/weatherapi"
+        cpu               = "1"
+        memory            = "1"
+
+        ports {
+        port              = 80
+        protocol          = "TCP"
+        }   
+  }
+
+}
