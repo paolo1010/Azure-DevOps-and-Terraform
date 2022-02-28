a0c4c86c5862017d0fc329341ce32f22e9cbc591
paul <paul.dohou@slalom.com>
2022-02-27 23:11:54 -0500

    updated pipeline file

--- main.tf
+++ main.tf
@@ -7,6 +7,16 @@ terraform {
   }
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tf_rg_blogstorage"
+    storage_account_name = "tfstorageagbonnon10"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
+
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
